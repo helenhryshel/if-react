ac64b7ed9b90ee7527480f19cb4ce6708d702644
helenhryshel <helenhryshel@gmail.com>
2023-05-26 10:19:43 +0300

    lesson-32: add changes

---
+++ src/components/Container/Container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './Container.css';
+
+// eslint-disable-next-line react/prop-types
+export const Container = ({ children, className }) => (
+  <div className={classNames('guests-container', className)}>
+    {children}
+  </div>
+);
